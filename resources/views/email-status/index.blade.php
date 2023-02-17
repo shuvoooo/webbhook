ab440ca928a05e7117f7fc83108efe644e42e031
shuvoooo <shuvo.punam@gmail.com>
2023-02-18 00:53:44 +0600

    bootstrap added

--- resources/views/email-status/index.blade.php
+++ resources/views/email-status/index.blade.php
@@ -1,20 +1,38 @@
-<table border="1" cellpadding="0" cellspacing="0">
-    <thead>
-    <tr>
-        <th>ID</th>
-        <th>To Email</th>
-        <th>Status</th>
-    </tr>
-    </thead>
-    <tbody>
-    @foreach ($data as $item)
-        <tr>
-            <td>{{ $item->id }}</td>
-            <td>{{ $item->to_email }}</td>
-            <td>{{ $item->status }}</td>
-        </tr>
-    @endforeach
-    </tbody>
-</table>
+<html>
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body>
 
-{{ $data->links() }}
+<div class="container">
+    <div class="row">
+        <div class="col mt-3">
+            <table border="1" cellpadding="0" cellspacing="0" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>To Email</th>
+                    <th>Status</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach ($data as $item)
+                    <tr>
+                        <td>{{ $item->id }}</td>
+                        <td>{{ $item->to_email }}</td>
+                        <td>{{ $item->status }}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+
+            {{ $data->links() }}
+        </div>
+    </div>
+</div>
+
+
+</body>
+</html>
+
